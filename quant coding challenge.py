69c4bc62c29472766426270d941be0cca7d9b31c
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2023-02-19 01:11:18 +0800

    Update quant coding challenge.py

--- quant coding challenge.py
+++ quant coding challenge.py
@@ -2,19 +2,28 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import yfinance as yf
+import streamlit as st
 
-#Get the input
-start = str(input("Enter your start: "))
-end = str(input("Enter your end: "))
-start_date = datetime.strptime(start, "%Y-%m-%d")
-end_date = datetime.strptime(end, "%Y-%m-%d")
+st.write("""
+# Stock Market Web Application
+**Test** show annualized return on a stock! 
+""")
+
+st.sidebar.header('User Input')
+
+st_assets = st.sidebar.text_input("Assets" ,'AAPL')
+st_start_date = st.sidebar.text_input("Start Date" ,'2021-12-31')
+st_end_date = st.sidebar.text_input("End Date" ,'2022-12-31')
+
+start_date = datetime.strptime(st_start_date, "%Y-%m-%d")
+end_date = datetime.strptime(st_end_date, "%Y-%m-%d")
 days = (end_date - start_date).days
-    
+
 #Tickers of assets
-assets = [str(input("Enter your assets: "))]
+assets = [st_assets]
 
 #Downloading data
-data = yf.download(assets, start = start, end = end)
+data = yf.download(assets, start = st_start_date, end = st_end_date)
 data = data['Adj Close']
 data.columns = assets
 
@@ -22,6 +31,6 @@ data.columns = assets
 def calculate_annualized_return(assets): 
     total_return = (data[-1] - data[0]) / data[0]
     annualized_return = ((1+total_return)**(365/days))-1
-    return(annualized_return)
+    st.write(annualized_return)
 
-print(calculate_annualized_return(assets))
+calculate_annualized_return(assets)
